bafbc132152ac3827287bcc171f930a6bb07022d
kevinzanzi <69643444+kevinzanzi@users.noreply.github.com>
2024-03-11 14:08:07 -0300

    Update RSS feed title in en/rss.xml.js

--- src/pages/en/rss.xml.js
+++ src/pages/en/rss.xml.js
@@ -2,11 +2,11 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Kizen Blog',
+    title: 'Aurum',
     description: 'Software developer with remote experience in frontend using React, JavaScript, TypeScript, and Tailwind css. I developed my own blog through Astro, which is available in Spanish (ar) and English (en) along with their respective rss feeds, and is deployed with Cloudflare.',
     site: context.site + '/en/',
     items: await pagesGlobToRssItems(
-      import.meta.glob('./blog/article/*.{md,mdx}')
+      import.meta.glob('./article/*.{md,mdx}')
     ),
     customData: `<language>en</language>`,
   });
